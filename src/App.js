3bd9220f61f875d71f276a6d84db894295090485
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-09 01:59:42 -0700

    frist

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {React,Fragment} from 'react';
+import Welcome from './Provreds/Welcome';
+function App() {
+  return (
+ <Fragment>
+   <Welcome/>
+
+ </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Provreds/Welcome.js
@@ -0,0 +1,15 @@
+ import {React,Fragment} from 'react';
+ import classes from './Wel.module.css'
+ const Welcome= (props)=>{
+
+    return (
+        <Fragment>
+            <div className={classes.index}>
+<h1></h1>
+            </div>
+
+        </Fragment>
+    )
+
+ }
+ export default Welcome
