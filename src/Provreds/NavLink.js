62d0f59484c5cf29fe8789061cf792b101e33f26
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-12 10:01:12 -0700

    work on the nav

---
+++ src/Provreds/NavLink.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+const Nav = () => {
+    return (
+        <Fragment>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+
+        </Fragment>
+    )
+}
+export default Nav;
