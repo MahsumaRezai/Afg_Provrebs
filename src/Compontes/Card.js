c6d088d47d166706ad1a7d31fc9edc7921904296
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-14 01:54:18 -0700

    work on the card section

---
+++ src/Compontes/Card.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './main.css'; // CSS file
+
+const Card = (props) => {
+    return (
+        <Fragment>
+            <div className="Card">
+            </div>
+        </Fragment>
+    )
+}
+
+export default Card
