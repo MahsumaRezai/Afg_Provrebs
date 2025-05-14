ca3e66bd0fb54cb2a897ad9d9b32304aaec13c0f
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-14 01:19:37 -0700

    work on the main section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {React,Fragment} from 'react';
+import { React, Fragment } from 'react';
 import Welcome from './Provreds/Welcome';
+import Main from './Compontes/Main/Main';
 function App() {
   return (
- <Fragment>
-   <Welcome/>
+    <Fragment>
+      <Welcome />
+      <Main />
 
- </Fragment>
+    </Fragment>
   );
 }
 
---
+++ src/Compontes/Main/Main.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+const Main = () => {
+    reuter(
+        <Fragment>
+            <h1>Hello Mahs</h1>
+
+        </Fragment>
+    )
+}
+
+export default Main;
