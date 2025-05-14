eea095ba7f3729d5c5ceb6631250794eccfbbcf3
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-14 01:15:34 -0700

    work on the router section

--- src/Provreds/NavLink.js
+++ src/Provreds/NavLink.js
@@ -1,10 +1,13 @@
-import { Fragment } from 'react'
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <Fragment>
             <nav>
                 <ul>
-                    <li></li>
+                    <li>
+                        <Link to="welcome"></Link>
+                    </li>
                 </ul>
             </nav>
 
