b5a2aa214813549f3dbb6858feda3505cb353854
Masoumeh <mahsuma.rezai8@gmail.com>
2025-05-14 02:25:39 -0700

    work on the crodu section

---
+++ src/Compontes/List.js
@@ -0,0 +1,18 @@
+const List = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>Hello, Masoumeh and you are best porp</li>
+            </ul>
+            <div>
+                <button>Add Provrebs</button>
+                <button>Edit Provrebs</button>
+                <button>Delete Provrebs</button>
+
+            </div>
+
+
+
+        </div>
+    )
+}
